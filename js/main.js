d85f739f2cdaefb6b9b7956a3253cee38ec814df
DhianelizLogreira <dhianelizlogreira@gmail.com>
2023-09-18 14:19:54 -0300

    SegundaPreEntrega

--- js/main.js
+++ js/main.js
@@ -1,16 +1,39 @@
+let nombreLibro = prompt("Escriba alguna de las dos opciones:\n- Marina \n- Le Tempo"); 
 
-let monto = parseFloat(prompt("Ingrese un monto"))
-let cuotas = parseFloat(prompt("Ingrese número de cuotas  máximo 5 cuotas"))
+function Libros(nombre, autor, precio) {
+  this.nombre = nombre;
+  this.autor = autor;
+  this.precio = precio;
+  this.descuento = 15;
+  this.aplicarDescuento = () => {
+    this.precio = this.precio - ((this.precio * this.descuento) / 100)
+  }
+}
 
-  
- for(let i = 1; i<=5; i++){
- if(cuotas <=5){
-  resultado = monto / cuotas
-        alert(`Monto a pagar por cuotas es: ${resultado}`)
- 
-}else{
-  alert(`Debe ingresar solo hasta 5 cuotas`)
-cuotas = parseFloat(prompt("Ingrese número de cuotas  máximo 5 cuotas"))
+function titulos (nombreLibro){
+if (nombreLibro == "Marina"){
+  return book1;
+} else if (nombreLibro == "Le Tempo"){
+  return book2;
 }
 }
 
+
+const book1 = new Libros ("Nombre: Marina", "Autor: Carlos Ruiz", 1200);
+const book2 = new Libros ("Nombre: Le Tempo", "Autor: Maria Palacios", 570); 
+
+book1.aplicarDescuento();
+console.log(book1.nombre, book1.autor, book1.precio); 
+book2.aplicarDescuento(),
+console.log(book2.nombre, book2.autor, book2.precio); 
+
+
+//Metodo de busqueda
+
+const livre = [
+  {titulo: "Marina", precio: 1200},
+  {titulo: "Le Tempo", precio: 570},
+]
+
+const resultado = livre.find(rosa=> rosa.titulo == "Marina");
+console.log(resultado);
